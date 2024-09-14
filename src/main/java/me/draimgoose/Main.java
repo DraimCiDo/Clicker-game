7e305fba31e9d2b09d529cbe3e34e37b2da0ee62
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2024-09-15 00:31:44 +0300

    Реализация создание .exe файла.

--- src/main/java/me/draimgoose/Main.java
+++ src/main/java/me/draimgoose/Main.java
@@ -2,23 +2,23 @@ package me.draimgoose;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 import me.draimgoose.ui.ClickerGameUI;
-import me.draimgoose.managers.GameState;
 
 public class Main extends Application {
     @Override
-    public void start(javafx.stage.Stage primaryStage) {
+    public void start(Stage primaryStage) {
         ClickerGameUI gameUI = new ClickerGameUI();
-        Scene scene = new Scene(gameUI.getMainPane(), 800, 600); // Размер окна можно изменить по необходимости
-        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm()); // Добавляем CSS-файл
+        Scene scene = new Scene(gameUI.getMainPane(), 800, 600);
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
 
         primaryStage.setTitle("Clicker Game");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        // Добавляем обработчик закрытия окна для сохранения состояния
+        // Обработчик закрытия для сохранения состояния
         primaryStage.setOnCloseRequest(event -> {
-            GameState.getInstance().saveState();
+            me.draimgoose.managers.GameState.getInstance().saveState();
         });
     }
 
